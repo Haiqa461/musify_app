6355b54e09be9c2605fcf1f74719213fde871aec
Haiqa461 <2022ee255@student.uet.edu.pk>
2023-08-31 19:55:58 +0500

    this is app's  reset screen

---
+++ lib/account_screens/reset_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _ResetScreen extends StatefulWidget {
+  const _ResetScreen({super.key});
+
+  @override
+  State<_ResetScreen> createState() => __ResetScreenState();
+}
+
+class __ResetScreenState extends State<_ResetScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
