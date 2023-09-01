4ba5684e9871dc61f073a49e4c40f5a4c9dfea32
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2023-09-01 12:11:33 +0500

    songs added

--- lib/playlists_screens/playlist1_screen.dart
+++ lib/playlists_screens/playlist1_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:musify_app/songs_screen/Song_screen.dart';
 
 class Playlist1 extends StatefulWidget {
   const Playlist1({super.key});
@@ -8,13 +9,33 @@ class Playlist1 extends StatefulWidget {
 }
 
 class _Playlist1State extends State<Playlist1> {
+  List<String> names = [
+    "song 1",
+    "song 2",
+    "song 3",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 1,
       ),
-      body: const SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: ListView.builder(
+          itemCount: names.length,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(names[index]),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Song1()));
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
---
+++ lib/songs_screen/Song_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Song1 extends StatefulWidget {
+  const Song1({super.key});
+
+  @override
+  State<Song1> createState() => _Song1State();
+}
+
+class _Song1State extends State<Song1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+      ),
+    );
+  }
+}
