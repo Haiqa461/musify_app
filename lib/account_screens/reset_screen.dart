43a2b7ec0e28f86c63d6e2c08db568ae4ccd5ff0
Haiqa461 <2022ee255@student.uet.edu.pk>
2023-09-04 10:16:07 +0500

    haiaq

--- lib/account_screens/reset_screen.dart
+++ lib/account_screens/reset_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class ResetScreen extends StatefulWidget {
@@ -11,10 +13,42 @@ class ResetScreen extends StatefulWidget {
 class __ResetScreenState extends State<ResetScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [],
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text('Reset', style: TextStyle(
+              fontFamily: 'Lalezar',
+              fontSize: 30,
+              fontWeight: FontWeight.normal,
+              ),),
+              
+              Text('Password' , style: TextStyle(
+               fontFamily: 'Lalezar',
+               fontSize: 30,
+               fontWeight: FontWeight.normal, 
+              ),),
+                 Padding(
+                   padding: const EdgeInsets.symmetric(horizontal:10 ),
+                   child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Add new Password",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide( width : 2) 
+                      ),
+                      fillColor:  const Color.fromRGBO(248, 248, 248, 0.092),
+                      filled: true
+                    ),
+                   ),
+                 )
+            ],
+       
+          ),
         ),
       ),
     );
